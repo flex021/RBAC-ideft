9098045e02b736c0749bcca9ba18ef9539a32ac0
flex021 <congthong2015@gmail.com>
2025-09-05 20:15:00 +0700

    update cors

--- api-rbac-permissons/src/config/corsOptions.js
+++ api-rbac-permissons/src/config/corsOptions.js
@@ -1,9 +1,17 @@
 export const corsOptions = {
-  origin: function (origin, callback) {
-    return callback(null, true)
-  },
-
+  origin: [
+    'http://localhost:5173',
+    'https://rbac-ideft.vercel.app'
+  ],
+  credentials: true,
   optionsSuccessStatus: 200,
-
-  credentials: true
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Authorization',
+    'Cookie'
+  ]
 }
