5fea2f6d202f19d7251d64ff90b21a5c5746e918
flex021 <congthong2015@gmail.com>
2025-09-07 14:40:44 +0700

    add VITE_API_URL

--- web-rbac-permissons/src/utils/constants.js
+++ web-rbac-permissons/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_ROOT = import.meta.env.VITE_API_URL || 'https://rbac-ideft.onrender.com'
+export const API_ROOT = import.meta.env.VITE_API_URL || 'http://localhost:8080'
 
 export const TAB_URLS = {
   DASHBOARD: 'dashboard',
